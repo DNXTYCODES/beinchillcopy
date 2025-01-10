48f7ee0e4bc0c9921c4e067526ab1a152c2c3678
MatthewCodez <mayodele113@gmail.com>
2025-01-10 12:42:02 +0100

    auth0Config

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -314,7 +314,7 @@ const tokenRequestData = {
   client_secret: clientSecret,
   audience: audience,
   grant_type: "authorization_code",
-  redirect_uri: "https://dnxtsolarprojecttt.onrender.com",
+  redirect_uri: "https://energygridshow.onrender.com",
   // grant_type: "client_credentials",
   
 };
