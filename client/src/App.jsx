519794fafc3c0796823578a27ed53a44ed5ec480
MatthewCodez <mayodele113@gmail.com>
2025-01-10 15:51:38 +0100

    cmtt

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,12 @@ import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Website from "./pages/Website";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-// import Properties from "./pages/Properties/Properties";
+import Properties from "./pages/Properties/Properties";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-// import Property from "./pages/Property/Property";
+import Property from "./pages/Property/Property";
 import UserDetailContext from "./context/UserDetailContext";
 // import Bookings from "./pages/Bookings/Bookings";
 // import Favourites from "./pages/Favourites/Favourites";
@@ -53,10 +53,10 @@ function App() {
               <Route element={<Layout />}>
                 <Route path="/" element={<Website />} />
                 <Route path="/adminpeople" element={<AdminPeople />} />
-                {/* <Route path="/properties">
+                <Route path="/properties">
                   <Route index element={<Properties />} />
                   <Route path=":propertyId" element={<Property />} />
-                </Route> */}
+                </Route>
                 {/* <Route path="/bookings" element={<Bookings />} /> */}
                 {/* <Route path="/favourites" element={<Favourites />} /> */}
                 {/* <Route path="/estimate" element={<Estimate />} /> */}
